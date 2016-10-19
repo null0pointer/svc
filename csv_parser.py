bbc666ea46f08334c8e6040587a79652ff209d8c
Sam Watson <swatson@MEL01-UMAC018.local>
2016-10-19 17:35:58 +1100

    Abstracted the CSV parsing

---
+++ csv_parser.py
@@ -0,0 +1,62 @@
+def parse_training_csv(csv):
+    # Set up arrays for features and targets
+    features = []
+    targets = []
+
+    # Open the training data CSV
+    f = open(csv, 'r')
+
+    # Split the CSV into lines
+    lines = f.read().split('\n')
+    f.close()
+
+    # Get the number of columns
+    columns = len(lines[0].split(','))
+
+    # Remove the title row from the lines
+    lines = lines[1:]
+
+    # Parse the lines into arrays and add to features and targets
+    for line in lines:
+        values = line.split(',')
+        if len(values) == columns:
+            targets.append(float(values[-1]))
+            features.append([float(x) for x in values[:-1]])
+            
+    return (features, targets)
+    
+def parse_prediction_csv(csv):
+    # Set up arrays for predictions
+    ids = []
+    features = []
+
+    # Open the training data CSV
+    f = open('numerai_tournament_data.csv', 'r')
+
+    # Split the CSV into lines
+    lines = f.read().split('\n')
+    f.close()
+
+    # Get the number of columns
+    columns = len(lines[0].split(','))
+
+    # Remove the title row from the lines
+    lines = lines[1:]
+
+    # Parse the lines into arrays and add to features and ids
+    for line in lines:
+        values = line.split(',')
+        if len(values) == columns:
+            ids.append(str(values[0]))
+            features.append([float(x) for x in values[1:]])
+            
+    return (ids, features)
+    
+def write_lines_to_csv(csv, lines):
+    # Write to CSV
+    f = open(csv, 'w')
+    for line in lines:
+        line = [str(x) for x in line]
+        f.write(','.join(line))
+        f.write('\n')
+    f.close()
--- svc.py
+++ svc.py
@@ -1,82 +1,29 @@
 from sklearn import svm
+import csv_parser
 
-# Set up arrays for features and targets
-training_features = []
-training_targets = []
-
-# Open the training data CSV
-f = open('numerai_training_data.csv', 'r')
-
-# Split the CSV into lines
-lines = f.read().split('\n')
-f.close()
-
-# Get the number of columns
-columns = len(lines[0].split(','))
-
-# Remove the title row from the lines
-lines = lines[1:]
-
-# Parse the lines into arrays and add to features and targets
-print('Parsing...')
-for line in lines:
-    values = line.split(',')
-    if len(values) == columns:
-        target = float(values[-1])
-        values = values[:-1]
-        features = [float(x) for x in values]
-        training_features.append(features)
-        training_targets.append(target)
+print('Parsing training data...')
+training_features, training_targets = csv_parser.parse_training_csv('numerai_training_data.csv')
 
 # Train the model
-print('Training...')
+print('Training model...')
 classifier = svm.SVC(probability=True, verbose=True)
-classifier.fit(training_features, training_targets)
+classifier.fit(training_features[:100], training_targets[:100])
 
-# Set up arrays for predictions
-prediction_ids = []
-prediction_features = []
-
-# Open the training data CSV
-f = open('numerai_tournament_data.csv', 'r')
-
-# Split the CSV into lines
-lines = f.read().split('\n')
-f.close()
-
-# Get the number of columns
-columns = len(lines[0].split(','))
-
-# Remove the title row from the lines
-lines = lines[1:]
-
-# Parse the lines into arrays and add to features and ids
-print('Parsing...')
-for line in lines:
-    values = line.split(',')
-    if len(values) == columns:
-        tid = str(values[0])
-        values = values[1:]
-        features = [float(x) for x in values]
-        prediction_ids.append(tid)
-        prediction_features.append(features)
+print('Parsing test data...')
+prediction_ids, prediction_features = csv_parser.parse_prediction_csv('numerai_tournament_data.csv')
         
 # Predict the outcomes
-predictions = classifier.predict_proba(prediction_features)
+print('Running predictions...')
+predictions = classifier.predict_proba(prediction_features[:100])
 
 # Set up the output array
 prediction_output = [['t_id', 'probability']]
 
 # Populate the output array
-for i in range(len(prediction_ids)):
+for i in range(len(prediction_ids[:100])):
     tid = prediction_ids[i]
     prediction = predictions[i][1]
     prediction_output.append([tid, prediction])
     
-# Write to CSV
-f = open('predictions.csv', 'w')
-for prediction in prediction_output:
-    prediction = [str(x) for x in prediction]
-    f.write(','.join(prediction))
-    f.write('\n')
-f.close()
+print('Writing predictions to csv...')
+csv_parser.write_lines_to_csv('predictions.csv', prediction_output)
