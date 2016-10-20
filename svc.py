7cad256968268e35af201acac09c1ffada64dd5d
Sam Watson <swatson@MEL01-UMAC018.local>
2016-10-20 13:26:41 +1100

    Removed limiting for tests

--- svc.py
+++ svc.py
@@ -7,20 +7,20 @@ training_features, training_targets = csv_parser.parse_training_csv('numerai_tra
 # Train the model
 print('Training model...')
 classifier = svm.SVC(probability=True, verbose=True)
-classifier.fit(training_features[:100], training_targets[:100])
+classifier.fit(training_features, training_targets)
 
 print('Parsing test data...')
 prediction_ids, prediction_features = csv_parser.parse_prediction_csv('numerai_tournament_data.csv')
         
 # Predict the outcomes
 print('Running predictions...')
-predictions = classifier.predict_proba(prediction_features[:100])
+predictions = classifier.predict_proba(prediction_features)
 
 # Set up the output array
 prediction_output = [['t_id', 'probability']]
 
 # Populate the output array
-for i in range(len(prediction_ids[:100])):
+for i in range(len(prediction_ids)):
     tid = prediction_ids[i]
     prediction = predictions[i][1]
     prediction_output.append([tid, prediction])
